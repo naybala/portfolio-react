25132ca870a997b2ef8ffb46358a4b0325302c14
naybala <naybalaismylife.nbl@gmail.com>
2022-07-15 13:03:44 +0630

    adding Loading Screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{Fragment ,useEffect , useState} from "react";
 import ScrollTop from "./Component/ScrollTop/ScrollTop";
 import Navbar from "./Component/Navbar";
 import ShowCase from "./Component/ShowCase";
@@ -8,20 +8,36 @@ import Tutorial from "./Component/Tutorial/Tuotrial";
 import Blog  from "./Component/Blog/Blog";
 import Community from "./Component/Community/Community";
 import Footer from "./Component/Footer/Footer";
+import Loading from "./Component/Loading/Loading";
 import "./App.css"
 function App() {
+  const [loader , setLoader] = useState(true);
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLoader(false);
+    },3000);
+  })
+
+
   return (
-    <div className="App">
-      <Navbar/>
-      <ShowCase/>
-      <SignUp/>
-      <Document/>
-      <Tutorial/>
-      <ScrollTop/>
-      <Blog/>
-      <Community/>
-      <Footer/>
-    </div>
+    <Fragment>
+      {loader ? 
+        <div className="App">
+        <Loading/>
+        </div> 
+        : 
+        <div className="App">
+          <Navbar/>
+          <ShowCase/>
+          <SignUp/>
+          <Document/>
+          <Tutorial/>
+          <ScrollTop/>
+          <Blog/>
+          <Community/>
+          <Footer/>
+        </div>}
+    </Fragment>
   );
 }
 export default App;
